70f2f1c217d897ac3f125866a6a849b7e22f3406
Manny <mannyflory9@gmail.com>
2018-11-30 18:22:49 -0500

    added more functions to backend

--- server/fitApp/controller.js
+++ server/fitApp/controller.js
@@ -7,5 +7,20 @@ const app = express.Router();
 app.get("/", (req,res) => {
     res.send(fitapp);
 });
+// login a new user
+app.post('/users', (req,res) => {
+    let user = fitapp.login(req.params.body, req.params.age);
+    res.send(user);
+});
+
+//finding a user using its name
+app.get('/find/:name', (req,res) => {
+    const name = fitapp.users.find(x => x.name === String(req.params.name));
+    if(!name){
+        res.status(404).send('Name Not Fouund');
+    }
+    res.send(name);
+});
+
 
 module.exports = app;
