6fd8d09b368a9080034ab203025da6ecb82e7ba6
Manny <mannyflory9@gmail.com>
2018-11-30 20:43:31 -0500

    changed nav.vue and style with css

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,38 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <router-link class="nav-link" exact-active-class="active" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link class="nav-link" exact-active-class="active" to="/about">About</router-link>
-            </li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/">Contact</a></li>
         </ul>
     </nav>
 </template>
 
 <style>
+ul{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color:red;
+}
+
+li{
+    display:inline-block;
+    margin:10px 20px;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    border: 1px solid black;
+}
+li a:hover {
+    background-color:black;
+}
+
 
 </style>
